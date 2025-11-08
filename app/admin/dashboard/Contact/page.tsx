dcd37ea3962a402bdd187baba1c5d451c343d3e3
Forinite <obefortune2@gmail.com>
2025-11-08 17:39:43 +0100

    Debuging Sessions SN4: Total Admin Sections Clean Up

--- app/admin/dashboard/Contact/page.tsx
+++ app/admin/dashboard/Contact/page.tsx
@@ -1,22 +1,18 @@
 // app/admin/dashboard/Contact/page.tsx
 
-import { getContactData } from '@/lib/sanity';
 import ContactSection from './ContactSection';
 
-import AdminNavbar from "@/app/(components)/adminComponents/AdminNavbar";
-
 export default async function ContactDashboard() {
-    const contactData = await getContactData();
+
 
     return (
         <div className="flex min-h-screen bg-gray-100 dark:bg-gray-900">
-            {/*/!* Sidebar *!/*/}
-            {/*<AdminNavbar />*/}
+
 
             {/* Main Content */}
             <main className="flex-1 md:p-8">
                 <h2 className="text-xl md:text-3xl font-bold text-gray-800 dark:text-gray-100 mb-6">Contact Dashboard</h2>
-                <ContactSection data={contactData} />
+                <ContactSection  />
             </main>
         </div>
     );
--- app/admin/dashboard/Gallery/page.tsx
+++ app/admin/dashboard/Gallery/page.tsx
@@ -1,6 +1,5 @@
 // app/admin/dashboard/Gallery/page.tsx
 
-import { getGalleryImages } from '@/lib/sanity';
 import GallerySection from './GallerySection';
 
 export default async function GalleryDashboard() {
